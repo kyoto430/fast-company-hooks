0717d19a9d0933701ff687607e3e77e37f8eee2e
ruslan prakapets <nateriver345@gmail.com>
2021-11-06 11:33:57 +0300

    children fix

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -3,13 +3,14 @@ import CollapseWrapper from "../common/collapse";
 import PropTypes from "prop-types";
 
 const ListNumbering = ({ children }) => {
-    return (
-        <ol>
-            {React.Children.map(children, (child) => {
-                return <li>{child}</li>;
-            })}
-        </ol>
-    );
+    return React.Children.map(children, (child, index) => {
+        return (
+            <div className="d-flex ps-3">
+                <span className="me-1">{index + 1}.</span>
+                {child}
+            </div>
+        );
+    });
 };
 
 ListNumbering.propTypes = {
